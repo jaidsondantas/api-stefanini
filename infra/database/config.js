c8666741192c39311e1744e65e1f8a6fbb503027
Jaidson Dantas <jaidsondantas@gmail.com>
2024-02-24 20:01:53 -0300

    fix: add config database

--- infra/database/config.js
+++ infra/database/config.js
@@ -2,8 +2,8 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({
     region: 'us-east-1',
-    accessKeyId: process.env.AWS_KEY_ID,
-    secretAccessKey: process.env.AWS_ACCESS_KEY
+    accessKeyId: 'AKIA6GBMH5WJLTDOXEPJ',
+    secretAccessKey: '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'
 });
 
 const dynamoDBClient =new AWS.DynamoDB.DocumentClient({region: 'us-east-1'})
