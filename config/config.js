8be8490bbfee182d4723a6aaacd7549455e01000
Jaidson Dantas <jaidsondantas@gmail.com>
2024-02-24 19:53:31 -0300

    fix: allow methods cors

--- config/config.js
+++ config/config.js
@@ -1,4 +1,16 @@
-const headers =
-    {
-        'Content-Type': 'application/json',
+const buildResponse = (statusCode, body) => {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+            "Access-Control-Allow-Credentials": true,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body),
     };
+};
+
+module.exports = buildResponse
+
